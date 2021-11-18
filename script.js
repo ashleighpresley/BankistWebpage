f635fb7a73bde512a637b9ae656aca7d24af89b5
ashleighpresley <ashleighpresley1@gmail.com>
2021-11-18 14:05:18 -0600

    Implement smooth scrolling for nav bar

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 
 ///////////////////////////////////////////////////////////
-// Modal window
+// Element selectors
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const header = document.querySelector('.header');
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -46,33 +52,23 @@ closeCookieBtn.addEventListener('click', function () {
 });
 
 ///////////////////////////////////////////////////////////
-//Smooth scrolling animation
-const allLinks = document.querySelectorAll('a:link');
+//Smooth scrolling animation nav bar
 
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
 
-    //Scoll to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-
-    //Scoll to selected section
-    if (href !== '#' && href.startsWith('#')) {
-      const section = document.querySelector(href);
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+//Smooth scrolling animation learn more button
 
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
 ///////////////////////////////////////////////////////////
